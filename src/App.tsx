6eeaab04ba6d6e70a421e07dda47f0b87eda7a38
Vilmantas Sudaris <vilmantas.sudaris@gmail.com>
2022-07-26 22:05:56 +0300

    prettier config fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 function App() {
   return (
     <>
-    <h1>TESTING</h1>
+      <h1>TESTING</h1>
     </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const container = document.getElementById('root')!;
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
@@ -14,9 +14,7 @@ root.render(
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<App />}>
-
-          </Route>
+          <Route path="/" element={<App />}></Route>
         </Routes>
       </BrowserRouter>
     </Provider>
