93183d90e0362b2c224185facd9ac772b63519cd
Vilmantas Sudaris <vilmantas.sudaris@gmail.com>
2022-08-15 22:21:56 +0300

    UPD | add data to hompePage

--- src/components/VotingTable/VotingTable.styles.ts
+++ src/components/VotingTable/VotingTable.styles.ts
@@ -1,10 +1,8 @@
 import { Theme } from "@mui/material";
 
 export default (theme: Theme) => ({
-  myGrid: {
-    margin: "auto",
-    maxWidth: 1440,
-    backgroundColor: "red",
-    marginTop: theme.spacing(5),
+  tableContainer: {
+    display: "flex",
+    paddingTop: theme.spacing(3),
   },
 });
--- src/components/VotingTable/VotingTable.tsx
+++ src/components/VotingTable/VotingTable.tsx
@@ -1,11 +1,16 @@
 import { Grid, useTheme } from "@mui/material";
+import TableEntryRow from "../TableEntryRow/TableEntryRow";
 import useStyles from "./VotingTable.styles";
+import data from "../../mockData/db.json";
 
 const VotingTable = () => {
   const classes = useStyles(useTheme());
+
   return (
-    <Grid container sx={classes.myGrid}>
-      My Grid Begins Here
+    <Grid container sx={classes.tableContainer}>
+      {data.EV22.map((row) => (
+        <TableEntryRow {...row} />
+      ))}
     </Grid>
   );
 };
